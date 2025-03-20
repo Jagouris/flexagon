a6dc92e09a51117501cf58436f90469bec2c6c73
Jagaurus <mitchellgraham1998@gmail.com>
2025-03-20 13:58:45 +0000

    Add files via upload
    pageQuery v0.0.5

--- pagequery.js
+++ pagequery.js
@@ -1,14 +1,19 @@
-// pageQuery version 0.0.1
+// pageQuery v0.0.5
 // MIT License
 // Copyright (c) 2025 Jagaurus (https://github.com/Jagaurus)
 
-var mousePos = {x: 0, y: 0};
-var prevMousePos = {x: 0, y: 0};
-var activeElement = null;
+let mousePos = {x: 0, y: 0};
+let prevMousePos = {x: 0, y: 0};
+let activeWindow = null;
+let windows = [];
 
 function init(){
 	document.addEventListener('mouseup', function(){
-		activeElement = null;
+		if(activeWindow){
+			activeWindow.element.style.userSelect = "auto";
+			
+			activeWindow = null;
+		}
 	}, false);
 
 	document.addEventListener('mousemove', function(e){
@@ -18,62 +23,59 @@ function init(){
 		mousePos.x = e.clientX;
 		mousePos.y = e.clientY;
 		
-		if(activeElement){
-			activeElement.x += mousePos.x-prevMousePos.x;
-			activeElement.y += mousePos.y-prevMousePos.y;
+		if(activeWindow){
+			activeWindow.x += mousePos.x-prevMousePos.x;
+			activeWindow.y += mousePos.y-prevMousePos.y;
 		
-			activeElement.element.style.left = activeElement.x;
-			activeElement.element.style.top = activeElement.y;
+			activeWindow.element.style.left = activeWindow.x;
+			activeWindow.element.style.top = activeWindow.y;
 		}
 	}, false);
+	
+	let windowElements = document.getElementsByTagName("WINDOW");
+	
+	for(element of windowElements){
+		if(element.id){
+			windows[element.id] = new Window(element);
+			windows[element.id].init();
+		}else{
+			let i = windows.push(new Window(element));
+			windows[i-1].init();
+		}
+	}
 }
 
 function Window(element){
-	this.element = document.querySelector(element);
-	this.grip;
-	this.x, this.y = 0;
-	this.style;
+	this.element = element;
+	this.x = 0;
+	this.y = 0;
 	this.pages = [];
 	this.currentPage;
 	
 	this.init = function(){
-		this.grip = this.element.children["grip"];
+		this.element.style.position = "fixed";
+		this.element.style.zIndex = 0;
 		
-		this.style = window.getComputedStyle(this.element);
-		
-		if(this.grip){
-			this.grip.addEventListener("mousedown", () => {
-				this.style = window.getComputedStyle(this.element);
+		for(element of this.element.getElementsByTagName("PAGE")){
+			let id = element.getAttribute("id");
+			
+			if(id){
+				this.pages[id] = element;
 				
-				this.x = parseInt(this.style.getPropertyValue("left"));
-				this.y = parseInt(this.style.getPropertyValue("top"));
-					
-				activeElement = this;
-			}, false);
-		}
-		
-		for(i of this.element.childNodes){
-			if(i.tagName == "PAGE"){
-				var name = i.getAttribute("name");
+				if(this.currentPage === undefined) this.currentPage = id;
+			}else{
+				id = this.pages.push(element);
 				
-				if(name){
-					this.pages[name] = i;
-					
-					if(!this.currentPage) this.currentPage = name;
-				}else{
-					this.pages.push(i);
-					
-					if(!this.currentPage) this.currentPage = 0;
-				}
-				
-				i.style.display = "none";
+				if(this.currentPage === undefined) this.currentPage = id-1;
 			}
+			
+			element.style.display = "none";
 		}
 		
 		this.pages[this.currentPage].style.display = "block";
 	}
 	
-	this.switchPage = function(id){
+	this.gotoPage = function(id){
 		if(this.pages[id]){
 			this.pages[this.currentPage].style.display = "none";
 			
@@ -81,7 +83,62 @@ function Window(element){
 			
 			this.pages[id].style.display = "block";
 		}else{
-			console.log("Page " + id + " doesn't exist");
+			console.log('"'+id+'"');
+			console.log("That page doesn't exist.");
 		}
 	}
+	
+	this.makeActive = function(){
+		let currentStyle = window.getComputedStyle(this.element);
+				
+		this.x = parseInt(currentStyle.getPropertyValue("left"));
+		this.y = parseInt(currentStyle.getPropertyValue("top"));
+		
+		for(id in windows){
+			windows[id].element.style.zIndex = 0;
+		}
+		
+		this.element.style.zIndex = 1;
+		this.element.style.userSelect = "none";
+		
+		activeWindow = this;
+	}
+}
+
+function findParentWindow(srcElement){
+	let parentNode = srcElement.parentNode;
+		
+	while(parentNode){
+		if(parentNode.tagName == "WINDOW"){
+			if(parentNode.id){
+				return parentNode.id;
+			}else{
+				for(id in windows){
+					if(windows[id].element == parentNode){
+						return id;
+					}
+				}
+			}
+		}
+		
+		parentNode = parentNode.parentNode;
+	}
+	
+	console.log("This element is not part of a window object");
+}
+
+function getWindow(id){
+	if(windows[id]) return windows[id];
+}
+
+function dragWindow(id = null){
+	if(!id) id = findParentWindow(event.srcElement);
+	
+	windows[id].makeActive();
+}
+
+function turnPage(page, id = null){
+	if(!id) id = findParentWindow(event.srcElement);
+	
+	windows[id].gotoPage(page);
 }
